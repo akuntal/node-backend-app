618daeccab7a677c2b9811f338a6c7eabdc08327
Alok Kuntal <alokkuntal@Aloks-MacBook-Pro.local>
2017-06-26 23:20:19 +0530

    mongo

--- src/app.js
+++ src/app.js
@@ -1,13 +1,36 @@
 var express = require('express'),
 router = express.Router();
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 var User = require('./models/user');
 
-mongoose.connect('mongodb://localhost:27017/users_test');
+var jsonParser = bodyParser.json()
+
+mongoose.connect('mongodb://alok:alok@ds135812.mlab.com:35812/users_test', (err)=>{
+	if(err) throw err;
+	console.log("successfully connected to mongodb!!!")	
+});
+
 
 router.get('/users',(req, res)=>{
-	res.send('Users list to send');
+	User.find({},[],(err,data)=>{
+		res.send(data);
+	});
+	
 })
+
+router.post('/user', jsonParser, (req, res)=>{
+	if (!req.body) return res.sendStatus(400);
+	
+	let user = new User(req.body);
+	user.save((err)=>{
+		if(err)res.send(err);
+		res.send(req.body)
+	})
+})
+
+
 
 module.exports = router;
 
--- src/models/user.js
+++ src/models/user.js
@@ -5,27 +5,33 @@ bcrypt = require('bcrypt'),
 SALT_WORK_FACTOR = 10;
 
 var userSchema = new Schema({
-	name:String,
 	username:{type:String, required:true, unique:true},
-	password:{type:String, required:true},
-	created_at:Date,
-	updated_at:Date
+	password:{type:String, required:true}
 })
 
-userSchema.pre('save',()=>{
+userSchema.pre('save',function(next){
 	let user = this;
 	if(!user.isModified('password')) return next();
+	
 	bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt)=>{
 		if(err) return next(err);
-		bcrypt.hash(user.password, salt, (err, hash)=>{
-			if(err) return next(err);
-			
+		
+		bcrypt.hash(user.password, salt, (error, hash)=>{
+			if(error) return next(error);
+
 			//overriding password text with hashed one
 			user.password = hash;
 			next();
 		})
 	})
 })
+
+userSchema.methods.comparePassword = function(candidatePassword, cb){
+	bcrypt.compare(candidatePassword, this.password, (err, isMatch)=>{
+		if(err) return cb(err);
+		cb(null, isMatch);
+	});
+}
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
